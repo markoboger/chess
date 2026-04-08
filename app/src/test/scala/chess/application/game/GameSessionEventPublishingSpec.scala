b83d19489001b5c674084d9e01be5e2208f5a984
marko.boger <marko.boger@gmail.com>
2026-04-08 13:58:16 +0200

    Add gateway and event coverage tests

--- app/src/test/scala/chess/application/game/GameSessionEventPublishingSpec.scala
+++ app/src/test/scala/chess/application/game/GameSessionEventPublishingSpec.scala
@@ -28,6 +28,26 @@ class GameSessionEventPublishingSpec extends AnyWordSpec with Matchers {
       events.map(_.eventType) shouldBe Vector("game_created", "move_applied")
       events.last.move shouldBe Some("e4")
     }
+
+    "publish rejection, fen load, and delete events" in {
+      val recordedRef = Ref.unsafe[IO, Vector[GameSessionEvent]](Vector.empty)
+      val publisher = new GameEventPublisher:
+        override def publish(event: GameSessionEvent): IO[Unit] =
+          recordedRef.update(_ :+ event)
+
+      val service = new GameSessionService(publisher)
+      val (gameId, _) = service.createGame().unsafeRunSync().toOption.get
+
+      service.makeMove(gameId, "e9").unsafeRunSync() shouldBe a[Left[?, ?]]
+      service
+        .loadFen(gameId, "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
+        .unsafeRunSync() shouldBe a[Right[?, ?]]
+      service.deleteGame(gameId).unsafeRunSync() shouldBe true
+
+      val events = recordedRef.get.unsafeRunSync().map(_.eventType)
+      events should contain("move_rejected")
+      events should contain("fen_loaded")
+      events should contain("game_deleted")
+    }
   }
 }
-
--- src/test/scala/chess/microservices/gateway/GatewayRoutesSpec.scala
+++ src/test/scala/chess/microservices/gateway/GatewayRoutesSpec.scala
@@ -1,6 +1,7 @@
 package chess.microservices.gateway
 
 import cats.effect.IO
+import cats.effect.Ref
 import cats.effect.unsafe.implicits.global
 import chess.microservices.shared.HealthResponse
 import org.http4s.*
@@ -12,13 +13,20 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class GatewayRoutesSpec extends AnyWordSpec with Matchers {
 
-  private val mockClient: Client[IO] =
-    Client.fromHttpApp(HttpApp.liftF(IO.pure(Response[IO](Status.Ok))))
+  private def makeApp() = {
+    val recordedUri = Ref.unsafe[IO, Option[Uri]](None)
+    val httpApp: HttpApp[IO] = HttpApp { (req: Request[IO]) =>
+      recordedUri.set(Some(req.uri)).as(Response[IO](Status.Ok))
+    }
+    val client =
+      Client.fromHttpApp(httpApp)
 
-  private val app = GatewayRoutes.routes(mockClient).orNotFound
+    (recordedUri, GatewayRoutes.routes(client).orNotFound)
+  }
 
   "GET /health" should {
     "return 200 with gateway service name" in {
+      val (_, app) = makeApp()
       val resp = app.run(Request[IO](Method.GET, uri"/health")).unsafeRunSync()
       resp.status shouldBe Status.Ok
       val body = resp.as[HealthResponse].unsafeRunSync()
@@ -29,28 +37,69 @@ class GatewayRoutesSpec extends AnyWordSpec with Matchers {
 
   "proxy routes" should {
     "forward POST /api/games to game service" in {
+      val (_, app) = makeApp()
       val resp = app.run(Request[IO](Method.POST, uri"/api/games")).unsafeRunSync()
       resp.status shouldBe Status.Ok
     }
 
     "forward GET /api/games/some-id to game service" in {
+      val (_, app) = makeApp()
       val resp = app.run(Request[IO](Method.GET, uri"/api/games/some-id")).unsafeRunSync()
       resp.status shouldBe Status.Ok
     }
 
     "forward DELETE /api/games/some-id to game service" in {
+      val (_, app) = makeApp()
       val resp = app.run(Request[IO](Method.DELETE, uri"/api/games/some-id")).unsafeRunSync()
       resp.status shouldBe Status.Ok
     }
 
     "forward GET / to UI service" in {
+      val (_, app) = makeApp()
       val resp = app.run(Request[IO](Method.GET, uri"/")).unsafeRunSync()
       resp.status shouldBe Status.Ok
     }
 
     "forward GET /some/path to UI service" in {
+      val (_, app) = makeApp()
       val resp = app.run(Request[IO](Method.GET, uri"/some/path")).unsafeRunSync()
       resp.status shouldBe Status.Ok
+    }
+
+    "forward GET /api/games with query parameters to the game service" in {
+      val (recordedUri, app) = makeApp()
+      val resp = app.run(Request[IO](Method.GET, uri"/api/games?limit=20&offset=5")).unsafeRunSync()
+
+      resp.status shouldBe Status.Ok
+      recordedUri.get.unsafeRunSync().map(_.renderString) shouldBe
+        Some(s"${GatewayConfig.gameServiceUrl.renderString}/games?limit=20&offset=5")
+    }
+
+    "forward PATCH /api/games/:id to the game service" in {
+      val (recordedUri, app) = makeApp()
+      val resp = app.run(Request[IO](Method.PATCH, uri"/api/games/game-1")).unsafeRunSync()
+
+      resp.status shouldBe Status.Ok
+      recordedUri.get.unsafeRunSync().map(_.renderString) shouldBe
+        Some(s"${GatewayConfig.gameServiceUrl.renderString}/games/game-1")
+    }
+
+    "forward GET /api/openings/lookup with query parameters to the game service" in {
+      val (recordedUri, app) = makeApp()
+      val resp = app.run(Request[IO](Method.GET, uri"/api/openings/lookup?fen=abc")).unsafeRunSync()
+
+      resp.status shouldBe Status.Ok
+      recordedUri.get.unsafeRunSync().map(_.renderString) shouldBe
+        Some(s"${GatewayConfig.gameServiceUrl.renderString}/openings/lookup?fen=abc")
+    }
+
+    "forward GET /some/path with query parameters to the UI service" in {
+      val (recordedUri, app) = makeApp()
+      val resp = app.run(Request[IO](Method.GET, uri"/some/path?view=compact")).unsafeRunSync()
+
+      resp.status shouldBe Status.Ok
+      recordedUri.get.unsafeRunSync().map(_.renderString) shouldBe
+        Some(s"${GatewayConfig.uiServiceUrl.renderString}/some/path?view=compact")
     }
   }
 }
